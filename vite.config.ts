e2b4d02bd929b975ba3572fc7844e6a75e1e237e
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2025-11-26 15:44:25 +0530

    refactor: update lottie manual chunk dependency from lottie-web to lottie-react

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
       output: {
         manualChunks: {
           framer: ['framer-motion'],
-          lottie: ['lottie-web'],
+          lottie: ['lottie-react'],
         },
       },
     },
